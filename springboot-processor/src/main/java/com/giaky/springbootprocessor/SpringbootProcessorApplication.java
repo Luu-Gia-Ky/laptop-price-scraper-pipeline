74aebf2c7c025cd1e5d13867a41f3c788965039d
Dragon-Ky <giaky200601@gmail.com>
2026-05-22 10:32:37 +0700

    feat(init): initialize Spring Boot processor service with Maven

---
+++ springboot-processor/src/main/java/com/giaky/springbootprocessor/SpringbootProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.giaky.springbootprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootProcessorApplication.class, args);
+    }
+
+}
---
+++ springboot-processor/src/test/java/com/giaky/springbootprocessor/SpringbootProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giaky.springbootprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
